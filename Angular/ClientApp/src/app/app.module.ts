65b0e327e3311702bcf17c9c0d8d4b8550b06207
Joe <you@example.com>
2020-03-03 14:17:57 +0700

    แก้ หน้าหลัก กับเอาnav-bar ด้านบนออก

--- Angular/ClientApp/src/app/app.module.ts
+++ Angular/ClientApp/src/app/app.module.ts
@@ -23,9 +23,9 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: '', component: FetchDataComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'HomeComponent', component: HomeComponent },
     ])
   ],
   providers: [],
